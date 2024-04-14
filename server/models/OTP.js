419414a1ec41dbff7ad7f7bf5ba742720aed6aca
apurva313 <apurvakumar313@gmail.com>
2024-04-15 02:52:02 +0530

    Update sendOTP model

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const mailSender = require("../utils/mailSender");
-
+const emailTemplate = require("../mail/templates/emailVerificationTemplate");
 const OTPSchema = new mongoose.Schema({
     email:{
         type:String,
@@ -21,8 +21,12 @@ const OTPSchema = new mongoose.Schema({
 //a function -> to send emails
 async function sendVerificationEmail(email, otp) {
     try{
-        const mailResponse = await mailSender(email, "Verification Email from StudyNotion", otp);
-        console.log("Email sent Successfully: ", mailResponse);
+        const mailResponse = await mailSender(
+			email,
+			"Verification Email",
+			emailTemplate(otp)
+		);
+        console.log("Email sent Successfully: ", mailResponse.response);
     }
     catch(error) {
         console.log("error occured while sending mails: ", error);
@@ -30,10 +34,16 @@ async function sendVerificationEmail(email, otp) {
     }
 }
 
-OTPSchema.pre("save", async function(next) {
-    await sendVerificationEmail(this.email, this.otp);
-    next();
-}) 
+// Define a post-save hook to send email after the document has been saved
+OTPSchema.pre("save", async function (next) {
+	console.log("New document saved to database");
+
+	// Only send an email when a new document is created
+	if (this.isNew) {
+		await sendVerificationEmail(this.email, this.otp);
+	}
+	next();
+});
 
 
 
